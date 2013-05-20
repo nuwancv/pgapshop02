a8ab10334bc9e828123b17d8f57ec7531dbe93ef
nuwancv <nuwancv@gmail.com>
2013-05-20 08:12:49 -0700

    Camera API and Contacts API used

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -10,6 +10,8 @@ var EmployeeView = function(employee){
 		this.el = $('<div/>');
 		
 		this.el.on('click', '.add-location-btn', this.addLocation);
+		this.el.on('click', '.add-contact-btn', this.addToContacts);
+		this.el.on('click', '.change-pic-btn', this.changePicture);
 	};
 	
 	this.addLocation=function(event){
@@ -23,6 +25,52 @@ var EmployeeView = function(employee){
 				alert('Error getting this location');
 			}
 		);
+	};
+	
+	
+	this.addToContacts = function(){
+		event.preventDefault();
+		console.log('addToContacts');
+		if(!navigator.contacts){
+			app.showAlert("Contacts API not supported", "Error");
+			return;
+		}	
+		
+		var contact = navigator.contacts.create();
+		contact.name = {giveName: employee.firstName, familyName: employee.lastName};
+		var phoneNumbers = [];
+		phoneNumber[0] = new ContactField('work', employee.officePhone, false);
+		phoneNumber[1] = new ContactField('mobile', employee.cellPhone, true); // Preferred Number
+		contact.phoneNumbers = phoneNnumbers;
+		contact.save();
+		return false;
+	};
+	
+	
+	this.changePicture = function(event) {
+		event.preventDefault();
+		if (!navigator.camera){
+			app.showAlert("Camera API not supported", "Error");
+			return;
+		}
+		
+		var options = { quality: 50,
+						destinationType: Camera.DestinationType.DATA_URL,
+						sourceType: 1, //0: Photo Library, 1=Camera, 2=Saved Photo Album
+						encordingType: 0
+					  };
+		
+		navigator.camera.getPicture(
+			function(imageData){
+				$('.employee-image', this.el).attr('src', "data:image/jpeg:based64," + imageData);
+			},
+			
+			function(){
+				app.showAlert('Error taking picture', 'Error');
+			},
+			options);
+			
+			return false;		
 	};
 	
 	this.render = function(){
